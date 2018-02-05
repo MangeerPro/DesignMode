8997636b51161c7cada4d71f8906dbd33687c232
cocky <smile.xiaohao@gmail.com>
2018-02-05 11:45:31 +0800

    懒加载线程安全，双重校验加锁

---
+++ src/main/java/com/cocky/singleton/lazy/treadsafe/Singleton.java
@@ -0,0 +1,32 @@
+package com.cocky.singleton.lazy.treadsafe;
+
+/**
+ * 线程安全的单例模式,双重校验加锁
+ */
+public class Singleton {
+    /**
+     * 存储单例对象实例
+     */
+    private volatile static Singleton instance = null;
+
+    private Singleton() {
+        //处理相关操作
+    }
+
+    /**
+     * 获取单例对象实例
+     */
+    public static Singleton getInstance() { 
+    			//先检查实例是否存在
+            if (instance == null) {
+            		//同步块，线程安全的创建实例
+            		synchronized(Singleton.class) {
+            			if(instance == null) {
+            			instance = new Singleton();
+            			}
+            		}
+            }
+        return instance;
+    }
+
+}
