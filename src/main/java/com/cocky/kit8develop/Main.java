dca53beb91ea6e7c47116d334c150bd267887561
cocky <smile.xiaohao@gmail.com>
2018-10-11 21:58:45 +0800

    change

--- src/main/java/com/cocky/kit8develop/Main.java
+++ src/main/java/com/cocky/kit8develop/Main.java
@@ -2,6 +2,8 @@ package com.cocky.kit8develop;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     
@@ -13,6 +15,9 @@ public class Main {
         for(String str: list) {
             System.out.println(str);
         }
-        list.stream().filter((p)->p=="One");
+        
+        list.stream().filter((p)->p=="One").forEach((s)->System.out.println(s));
+        Map<String,String> map = list.stream().collect(Collectors.toMap((String p)->p, (String p)->p));
+        System.out.println(map.toString());
     }
 }
