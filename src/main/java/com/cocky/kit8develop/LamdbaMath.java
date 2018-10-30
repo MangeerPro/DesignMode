6cfd41f1a542a1cce2740d01d72c4889845e95a1
cocky <smile.xiaohao@gmail.com>
2018-10-31 01:11:16 +0800

    Lamdba

--- src/main/java/com/cocky/kit8develop/LamdbaMath.java
+++ src/main/java/com/cocky/kit8develop/LamdbaMath.java
@@ -1,10 +1,35 @@
 package com.cocky.kit8develop;
 
+import java.util.Date;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntSupplier;
+
 public class LamdbaMath {
-	public void lamdbaFunction(String str) {
-		(String str)->{
-			System.out.println(str);
+	public void lamdbaFunction() {
+		Consumer<Integer> integerConsumer = (Integer times) -> {
+			System.out.println(times);
 		};
+
+		Consumer<Integer> comsumer = (Integer counts) ->{
+			++counts;
+		};
+		Function<Integer, Integer> integerFunction = (Integer count) -> {
+			return ++count;
+		};
+		BooleanSupplier booleanSupplier = () -> {
+			return 1 == 0;
+		};
+		IntSupplier intSupplier = () -> {
+			return 100;
+		};
+
+		comsumer.andThen(integerConsumer).accept(100);
+		integerConsumer.accept(90);
+		System.out.println(integerFunction.apply(100));
 	}
+
+
 
 }
