a04f44783274a67ec907bb77b9022d734b57e618
cocky <smile.xiaohao@gmail.com>
2018-10-31 00:44:26 +0800

    LamdbaMath

---
+++ src/main/java/com/cocky/kit8develop/LamdbaMath.java
@@ -0,0 +1,10 @@
+package com.cocky.kit8develop;
+
+public class LamdbaMath {
+	public void lamdbaFunction(String str) {
+		(String str)->{
+			System.out.println(str);
+		};
+	}
+
+}
--- src/main/java/com/cocky/kit8develop/Main.java
+++ src/main/java/com/cocky/kit8develop/Main.java
@@ -8,16 +8,7 @@ import java.util.stream.Collectors;
 public class Main {
     
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("One");
-        list.add("Two");
-        list.add("Three");
-        for(String str: list) {
-            System.out.println(str);
-        }
-        
-        list.stream().filter((p)->p=="One").forEach((s)->System.out.println(s));
-        Map<String,String> map = list.stream().collect(Collectors.toMap((String p)->p, (String p)->p));
-        System.out.println(map.toString());
+    	LamdbaMath lamdbaMath = new LamdbaMath();
+    	lamdbaMath.lamdbaFunction();
     }
 }
